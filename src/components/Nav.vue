775b39aad1bf88acd3330d9e4ab0f5039ac186a5
Unknown <kimwinner90@gmail.com>
2020-05-22 18:49:26 +0900

    add basic navbar

---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>   
+    <h2>Logo</h2>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav {
+  height: 60px;
+  border-bottom: 1px solid #9b9b9b
+}
+
+h2 {
+  line-height: 60px;
+  font-weight: 900;
+  margin-left: 30px;  
+}
+</style>

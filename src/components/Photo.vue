022eb9fafb559b052dac34b47557684b3fb2a63f
Unknown <kimwinner90@gmail.com>
2020-05-24 19:52:49 +0900

    init photo component

---
+++ src/components/Photo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="item-container">
+    <img :src="photoUrl" loading="lazy" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Photo',
+  props: {
+    photoUrl: String,
+  },
+  method: {
+
+  } 
+}
+</script>
+
+<style scoped>
+  div {
+    flex: 1;
+    height: 200px;
+  }
+
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+</style>

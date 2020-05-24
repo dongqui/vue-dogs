8722ccab5b27378bc899e7099ebf6b8329fd880d
Unknown <kimwinner90@gmail.com>
2020-05-24 19:45:30 +0900

    get dogs by breed api

--- src/api/api.dogs.js
+++ src/api/api.dogs.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
 export const fetchBreedsApi = async () => await axios.get('https://dog.ceo/api/breeds/list/all');
+export const fetchDogsByBreedApi = async (breed) => await axios.get(`https://dog.ceo/api/breed/${breed}/images`);
